7f904f0128b64fbf609e758ba3a30b2aad08b31f
saeed <saeid.aghdam@gmail.com>
2025-09-13 20:00:28 +0330

    Fix 'mul' filter error by adding custom math_filters template tags

---
+++ store_analysis/templatetags/math_filters.py
@@ -0,0 +1,35 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def mul(value, arg):
+    """Multiply value by arg"""
+    try:
+        return float(value) * float(arg)
+    except (ValueError, TypeError):
+        return 0
+
+@register.filter
+def div(value, arg):
+    """Divide value by arg"""
+    try:
+        return float(value) / float(arg)
+    except (ValueError, TypeError, ZeroDivisionError):
+        return 0
+
+@register.filter
+def add(value, arg):
+    """Add value and arg"""
+    try:
+        return float(value) + float(arg)
+    except (ValueError, TypeError):
+        return 0
+
+@register.filter
+def sub(value, arg):
+    """Subtract arg from value"""
+    try:
+        return float(value) - float(arg)
+    except (ValueError, TypeError):
+        return 0
